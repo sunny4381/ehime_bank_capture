b48dde247c3c46c00f5253d77bb6d3e486a9a9bf
NAKANO Hideo <pinarello.marvel@gmail.com>
2020-06-21 20:01:53 +0900

    clicks logout button after caputured page

--- capture.rb
+++ capture.rb
@@ -15,14 +15,19 @@ def set_target_self_and_click(path)
   @browser.at_css(path).click
 end
 
-
 @browser.goto("https://www.himegin.co.jp/security/index.php?id=wyn")
 set_target_self_and_click("#contents > div > p.btn-base.btn-default.btn-next.btn-icon-l.w50p.sp-w80p.mA.mt2 > a")
 
+# ログイン
 @browser.at_css("[name='BTX0010']").focus.type(ENV['USER_ID'])
 @browser.at_css("[name='BPW0020']").focus.type(ENV['PASSWORD'])
 @browser.at_css("[name='forward_BSM2010']").click
 
+# 撮影
 sleep 10
 @browser.screenshot(path: ENV["OUTPUT"] || "account.png")
+
+# ログアウト
+@browser.at_css("[name='forward_BSM0001']").click
+
 @browser.quit
